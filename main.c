5fbfa92742837e954df58e390e321dc7ec03b248
pmg532690 <pmg532690@sookmyung.ac.kr>
2025-10-07 19:19:27 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,22 +4,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 
-void func(void){
-	int x;
+void func(int x){
+
 	printf("func x is at %p\n", &x);
-	func2();
-}
-
-void func2(void){
-	int x;
-	printf("func2 x is at %p\n", &x);
 
 }
 
-int main(void){
+
+int main(int argc, char *argv[]){
 	int x;
 	printf("main x is at %p\n", &x);
-	func();
+	func(x);
 
 
 	return 0;
